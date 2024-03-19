a1d179e078d1819d9cad411218b657497adff6d7
Etec Prof. Horácio Augusto da Silveira <leo>
2024-03-19 12:42:11 -0300

    Criação Calculadora

---
+++ TipoCalculator/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace TipoCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void PercentageSlider_ValueChanged(object sender, EventArgs e)
+        {
+            TipoPorcentageLabel.Text = $" { PercentageSlider.Value.ToString()} %";
+        }
+
+        public void Button15Percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Button30Percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ButtonRoundDown_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        public void ButtonRoundUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
